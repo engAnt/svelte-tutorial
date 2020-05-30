a81a3b017a56cae4a600c6dc9b001909b8ba4a45
engAnt <codersellu@outlook.com>
2020-05-30 02:25:23 -0500

    reactivity: updating arrays and objects

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,12 @@
 	function handleClick() {
 		count += 1;
 	}
+
+	let numbers = [1, 2, 3, 4];
+	function addNumber() {
+		numbers = [...numbers, numbers.length + 1];
+	}
+	$: sum = numbers.reduce((t, n) => t + n, 0);
 </script>
 
 <main>
@@ -27,10 +33,17 @@
 	<img {src} alt="{alt_name} dances.">
 </main>
 
+<hr/>
 <button on:click={handleClick}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
 <p>{count} doubled is {doubled}</p>
+
+<hr/>
+<p>{numbers.join(' + ')} = {sum}</p>
+<button on:click={addNumber}>
+	Add a number
+</button>
 
 <style>
 	main {
