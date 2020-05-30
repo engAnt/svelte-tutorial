6edfe0b7ac25dd93521c20b5fc66bbc43ce8591e
engAnt <codersellu@outlook.com>
2020-05-30 03:19:34 -0500

    Logic: else-if blocks

--- src/LogicTuts.svelte
+++ src/LogicTuts.svelte
@@ -4,6 +4,8 @@
     function toggle() {
         user.loggedIn = !user.loggedIn;
     }
+
+    let x = 7;
 </script>
 
 {#if user.loggedIn}
@@ -14,4 +16,12 @@
   <button on:click={toggle}>
     Log in
   </button>
+{/if}
+
+{#if x > 10}
+  <p>{x} is greater than 10</p>
+{:else if 5 > x}
+  <p>{x} is less than 5</p>
+{:else}
+  <p>{x} is between 5 and 10</p>
 {/if}
