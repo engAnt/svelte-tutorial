fa5cd3ab49d0b85d59f678040b5a90a4fda1df3b
engAnt <codersellu@outlook.com>
2020-05-31 05:09:52 -0500

    Bindings: select bindings

--- src/BindingsTuts.svelte
+++ src/BindingsTuts.svelte
@@ -27,11 +27,27 @@
 
 
 	let value = `Some words are *italic*, some are **bold**`;
+
+
+	let questions = [
+		{ id: 1, text: `Where did you go to school?` },
+		{ id: 2, text: `What is your mother's name?` },
+		{ id: 3, text: `What is another personal fact that an attacker could easily find with Google?` }
+	];
+
+	let selected;
+
+	let answer = '';
+
+	function handleSubmit() {
+		alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`);
+	}
 </script>
 
 
 <style>
 	textarea { width: 100%; height: 200px; }
+	input { display: block; width: 500px; max-width: 100%; }
 </style>
 
 
@@ -69,7 +85,7 @@
 </button>
 
 
-<h2>Size</h2>
+<h3>Size</h3>
 
 <label>
 	<input type=radio bind:group={scoops} value={1}>
@@ -86,7 +102,7 @@
 	Three scoops
 </label>
 
-<h2>Flavours</h2>
+<h3>Flavours</h3>
 
 {#each menu as flavour}
 	<label>
@@ -106,7 +122,30 @@
 	</p>
 {/if}
 
+
 <br/><br/>
 <textarea bind:value></textarea>
 
 {@html marked(value)}
+
+
+<br/><br/>
+<h3>Insecurity questions</h3>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<select bind:value={selected} on:change="{() => answer = ''}">
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
+
+	<input bind:value={answer}>
+
+	<button disabled={!answer} type=submit>
+		Submit
+	</button>
+</form>
+
+<p>selected question {selected ? selected.id : '[waiting...]'}</p>
