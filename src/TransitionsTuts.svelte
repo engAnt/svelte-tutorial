973a67fbb1b613777bb020393398dfa1e2e26e46
engAnt <codersellu@outlook.com>
2020-05-31 10:49:34 -0500

    Transitions: custom js transitions

--- src/TransitionsTuts.svelte
+++ src/TransitionsTuts.svelte
@@ -1,6 +1,27 @@
 <script>
-	import { fade, fly } from 'svelte/transition';
-	let visible = true;
+	let visible = false;
+
+	function typewriter(node, { speed = 50 }) {
+		const valid = (
+			node.childNodes.length === 1 &&
+			node.childNodes[0].nodeType === Node.TEXT_NODE
+		);
+
+		if (!valid) {
+			throw new Error(`This transition only works on elements with a single text node child`);
+		}
+
+		const text = node.textContent;
+		const duration = text.length * speed;
+
+		return {
+			duration,
+			tick: t => {
+				const i = ~~(text.length * t);
+				node.textContent = text.slice(0, i);
+			}
+		};
+	}
 </script>
 
 <label>
@@ -9,7 +30,7 @@
 </label>
 
 {#if visible}
-	<p in:fly="{{ y: 200, duration: 2000 }}" out:fade>
-		Flies in, fades out
+	<p in:typewriter>
+		The quick brown fox jumps over the lazy dog
 	</p>
 {/if}
