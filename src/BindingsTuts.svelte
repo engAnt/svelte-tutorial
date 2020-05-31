5cc02cde1508b58d42fafe22f98f47538ea23475
engAnt <codersellu@outlook.com>
2020-05-30 23:47:36 -0500

    Bindings: numeric inputs

--- src/BindingsTuts.svelte
+++ src/BindingsTuts.svelte
@@ -1,7 +1,24 @@
 <script>
 	let name = 'world';
+
+
+	let a = 1;
+	let b = 2;
 </script>
 
 <input bind:value={name}>
 
 <h4>Hello {name}!</h4>
+
+
+<label>
+	<input type=number bind:value={a} min=0 max=10>
+	<input type=range bind:value={a} min=0 max=10>
+</label>
+
+<label>
+	<input type=number bind:value={b} min=0 max=10>
+	<input type=range bind:value={b} min=0 max=10>
+</label>
+
+<p>{a} + {b} = {a + b}</p>
